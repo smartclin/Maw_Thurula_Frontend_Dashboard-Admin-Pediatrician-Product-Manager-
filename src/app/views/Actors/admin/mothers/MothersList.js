fb9277233b09de00b3e3276f2077f18b29e2d36f
Hansana Ranaweera <73303406+ranaHP@users.noreply.github.com>
2022-07-23 16:44:49 +0530

    This commit for add material ui

--- src/app/views/Actors/admin/mothers/MothersList.js
+++ src/app/views/Actors/admin/mothers/MothersList.js
@@ -1,7 +1,9 @@
 import { Card, Grid, styled, useTheme } from '@mui/material';
 import { Fragment } from 'react';
 import AdminStatCards from "../../shared/admin/AdminStatCards";
+import DateTimePicker from '@mui/x-date-pickers-pro/DateTimePicker';
 
+import Button from '@mui/material/Button';
 
 const MothersList = () => {
 
@@ -16,6 +18,8 @@ const MothersList = () => {
             <h1> Mother's List HI Akhila</h1>
 
             <AdminStatCards cardList={cardList} />
+            <Button variant="contained">Hello World</Button>;
+            <DateTimePicker></DateTimePicker>
         </Fragment>
     );
 };
