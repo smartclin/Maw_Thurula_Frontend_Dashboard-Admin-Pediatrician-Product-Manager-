2ce6da5148f09a4380edad6ba1c11bd53bccdb05
akilaanjanadissanayaka <akilaanjanadissanayaka@gmail.com>
2022-07-24 22:21:58 +0530

    This commit for MotherListTable.jsx v2

--- src/app/views/Actors/admin/astrologers/Astrologers.js
+++ src/app/views/Actors/admin/astrologers/Astrologers.js
@@ -1,13 +1,18 @@
-import { Card, Grid, styled, useTheme } from '@mui/material';
+import {Button, Card, Grid, styled, useTheme} from '@mui/material';
 import { Fragment } from 'react';
 
 
 const Astrologers = () => {
 
-
+    const ContentBox = styled('div')(({ theme }) => ({
+        margin: '30px',
+        [theme.breakpoints.down('sm')]: { margin: '16px' },
+    }));
     return (
         <Fragment>
-            <h1> Astrologers List </h1>
+            <h1>astrologer</h1>
+            {/*<Example/>*/}
+
         </Fragment>
     );
 };
--- src/app/views/Actors/shared/mother/MotherListTable.jsx
+++ src/app/views/Actors/shared/mother/MotherListTable.jsx
@@ -1,89 +1,103 @@
-import { Box } from '@mui/material';
-import { MatxProgressBar, SimpleCard } from 'app/components';
-import { Small } from 'app/components/Typography';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import LockOpenOutlinedIcon from '@mui/icons-material/LockOpenOutlined';
 import MaterialTable from "material-table";
-const data=[
-        { name: 'Mehmet', posts: 10, comments: 1987, },
-        { name: 'Zerya Betül Zerya Betül Zerya Betül Zerya Betül', posts: 23, comments: 2017, },
-    { name: 'Mehmet', posts: 10, comments: 1987, },
-    { name: 'Zerya Betül Zerya Betül Zerya Betül Zerya Betül', posts: 23, comments: 2017, },
-    { name: 'Mehmet', posts: 10, comments: 1987, },
-    { name: 'Zerya Betül Zerya Betül Zerya Betül Zerya Betül', posts: 23, comments: 2017, },
-    { name: 'Mehmet', posts: 10, comments: 1987, },
-    { name: 'Zerya Betül Zerya Betül Zerya Betül Zerya Betül', posts: 23, comments: 2017, },
-    { name: 'Mehmet', posts: 10, comments: 1987, },
-    { name: 'Zerya Betül Zerya Betül Zerya Betül Zerya Betül', posts: 23, comments: 2017, },
-    { name: 'Mehmet', posts: 10, comments: 1987, },
-    { name: 'Zerya Betül Zerya Betül Zerya Betül Zerya Betül', posts: 23, comments: 2017, },
-    { name: 'Mehmet', posts: 10, comments: 1987, },
-    { name: 'Zerya Betül Zerya Betül Zerya Betül Zerya Betül', posts: 23, comments: 2017, },
-]
+// import React from 'react'
+import LockIcon from '@mui/icons-material/LockRounded';
+import LockOpenIcon from '@mui/icons-material/LockOpenRounded';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import MothersCards from "./Motherscard";
+
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+
+
 const MotherList = () => {
 
+    const [open, setOpen] = React.useState(false);
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    const checkStatus=(data)=>{
+        console.log("data - "+data)
+        return true
+    }
     return (
-        <MaterialTable
-            title="Registered Mothers List"
-            columns={[
-              { title: 'Name', field: 'name',
-                  headerStyle: {
-                      // backgroundColor: '#039be5',
-                      paddingLeft:30
-                  },
-                  cellStyle: {
-                      textAlign: "left",
-                      paddingLeft:30
+        <div>
+            <Dialog open={open} onClose={handleClose}>
+                <DialogTitle>Reason for this action</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        This message will show on this users window
+                    </DialogContentText>
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        id="reason"
+                        label="Reason for Block"
+                        type="text"
+                        fullWidth
+                        variant="standard"
+                    />
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose}>Cancel</Button>
+                    <Button onClick={handleClose}>Submit</Button>
+                </DialogActions>
+            </Dialog>
 
-                  }
+            {/*//#############################################################33*/}
 
-              },
-              { title: 'Posts', field: 'posts',type: 'numeric',
-                  headerStyle: {
-                      // backgroundColor: '#1a5d37',
-                      textAlign: "left"
-                  },
-                  cellStyle: {
-                      textAlign: "left",
-                      paddingLeft:30
-
-                  }
-              },
-              { title: 'Comments', field: 'comments', type: 'numeric',
-                  headerStyle: {
-                      // backgroundColor: '#dde503',
-                      textAlign: "left"                  },
-                  cellStyle: {
-                      minWidth: 10,
-                      maxWidth: 10,
-                      textAlign: "left",
-                      paddingLeft:30
-                  }
-              },
-            ]}
-            data={data}
-            actions={[
-              {
-                icon: LockOutlinedIcon,
-                tooltip: 'Save User',
-                onClick: (event, rowData) => alert("You saved " + rowData.name)
-              },
-              {
-                icon: LockOpenOutlinedIcon,
-                tooltip: 'Delete User',
-                onClick: (event, rowData) => alert("You want to delete " + rowData.name)
-              }
-            ]}
-            options={{
-                rowStyle: {
-                    fontSize:12.5,
-                    // color:'#91909E'
-                },
-
-
-            }}
-        />
+            <MaterialTable
+                title="Mothers List"
+                columns={[
+                    {
+                        field: 'url',
+                        title: 'Avatar',
+                        render: rowData => <img src={rowData.url} style={{width: 30, borderRadius: '50%',boxShadow: "0px 3px 6px #9E9E9E"}}/>,
+                        cellStyle: {
+                            paddingLeft:30
+                        },
+                        headerStyle: {
+                            paddingLeft:30
+                        }                },
+                    { title: 'Name', field: 'name',width: "10%" },
+                    { title: 'Email', field: 'Email',width: "10%" },
+                    { title: 'Posts', field: 'Posts',width: "10%" },
+                    { title: 'Comments', field: 'Comments',width: "10%" },
+                    { title: 'Status', field: 'Status',lookup:{0:'Unblock',1:'Block'},width: "10%" },
+                ]}
+                data={[
+                    {url:'https://avatars0.githubusercontent.com/u/7895451?s=460&v=4', name: 'Akila Anjana', Email: 'anjanadissanayaka@gmail.com', Posts: 1987, Comments: 63 ,Status:1},
+                    { url:'https://avatars0.githubusercontent.com/u/7895451?s=460&v=4',name: 'Anura Anjana', Email: 'anjanadissanayaka@gmail.com', Posts: 1987, Comments: 63 ,Status:1},
+                    { url:'https://avatars0.githubusercontent.com/u/7895451?s=460&v=4',name: 'Kasun Anjana', Email: 'anjanadissanayaka@gmail.com', Posts: 1987, Comments: 63 ,Status:0},
+                    { url:'https://avatars0.githubusercontent.com/u/7895451?s=460&v=4',name: 'Pasan Anjana', Email: 'anjanadissanayaka@gmail.com', Posts: 1987, Comments: 63 ,Status:1},
+                ]}
+                onRowClick={(event, rowData) => console.log(rowData)}
+                actions={[
+                    (rowData) => {
+                        return rowData.Status
+                            ? { icon: LockIcon, onClick: (rowData) => { /* anythink */ } }
+                            : { icon: LockOpenIcon, onClick: (rowData) => { handleClickOpen() } }
+                    },
+                    {
+                        icon: VisibilityIcon,
+                        tooltip: 'Save User',
+                        onClick: (event, rowData) => alert("You saved " + rowData.name),
+                    }
+                ]}
+                options={{sorting:true, exportAllData:true ,exportButton:true ,actionsColumnIndex: -1}}
+            />
+        </div>
     )
-};
-
+}
 export default MotherList;
