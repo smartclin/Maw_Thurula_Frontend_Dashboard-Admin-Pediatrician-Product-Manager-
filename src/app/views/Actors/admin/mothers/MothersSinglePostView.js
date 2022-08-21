32f921c998b6695e125c9b786022940a05157a47
akilaanjanadissanayaka <akilaanjanadissanayaka@gmail.com>
2022-08-21 18:08:28 +0530

    This commit for MothersSinglePostView.js Responsive issue

--- src/app/views/Actors/admin/mothers/MothersSinglePostView.js
+++ src/app/views/Actors/admin/mothers/MothersSinglePostView.js
@@ -35,28 +35,17 @@ const MothersList = () => {
     return (
         <Fragment>
             <ContentBox className="analytics">
-                {/*{*/}
-                {/*    motherList ? motherList.students.map((mother, index) => {*/}
-                {/*        return (*/}
-                {/*            <>*/}
-                {/*                /!*<h2>kkkkkkkk</h2>*!/*/}
-                {/*                <h1 key={index}> {mother.first_name} - {}</h1>*/}
-                {/*                /!*<img key={"2" + index} src={mother.url} width={"100px"}/>*!/*/}
-                {/*            </>)*/}
-
-                {/*    }) : <h1> Data 1 Loading</h1>*/}
-                {/*}*/}
-                <Grid container spacing={3}>
-                    <Grid item lg={6} md={1} sm={12} xs={12}>
+                <Grid container spacing={3} >
+                    <Grid item lg={6} md={6} sm={12} xs={12}>
                         <MotherPostblockcard/>
                     </Grid>
 
-                    <Grid item lg={6} md={4} sm={12} xs={12}>
+                    <Grid item lg={6} md={6} sm={12} xs={12}>
 
                         <MotherPostDescription/>
 
                     </Grid>
-                    <Grid item lg={12} md={4} sm={12} xs={12}>
+                    <Grid item lg={12} md={12} sm={12} xs={12}>
                         <AdminPostCommentMother/>
                     </Grid>
 
