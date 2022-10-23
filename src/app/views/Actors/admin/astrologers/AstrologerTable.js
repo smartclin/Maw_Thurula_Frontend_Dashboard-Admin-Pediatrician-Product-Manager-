939bc788b26968120e4025d6d89ad0ac12d60db4
akilaanjanadissanayaka <akilaanjanadissanayaka@gmail.com>
2022-10-23 16:07:08 +0530

    This commit for pediatrician, Astrologer, NP registration request

--- src/app/views/Actors/admin/astrologers/AstrologerTable.js
+++ src/app/views/Actors/admin/astrologers/AstrologerTable.js
@@ -20,6 +20,24 @@ import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 
 
 import Button from '@mui/material/Button';
+import Dialog from "@mui/material/Dialog";
+import DialogTitle from "@mui/material/DialogTitle";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogActions from "@mui/material/DialogActions";
+import * as React from "react";
+import {
+    AcceptPediatrician,
+    RejectPediatrician, View_Target_Pediatrician
+} from "../../../../services/Admin/Pediatrician/admin_pediatrician_service";
+import {useParams} from "react-router";
+import {useEffect, useState} from "react";
+import {
+    AcceptAstrologer,
+    RejectAstrologer,
+    View_Target_Astrologer
+} from "../../../../services/Admin/Astrologer/admin_astrologer_service";
+import {useNavigate} from "react-router-dom";
 
 const CardHeader = styled(Box)(() => ({
     display: 'flex',
@@ -63,98 +81,149 @@ const Small = styled('small')(({ bgcolor }) => ({
 
 
 const AstrologerTable = () => {
+    const [open, setOpen] = React.useState(false);
+    const [astrologerList, setastrologerList] = useState([]);
     const { palette } = useTheme();
-    const bgError = palette.error.main;
-    const bgPrimary = palette.primary.main;
-    const bgSecondary = palette.secondary.main;
+    let { id } = useParams();
+    const navigate = useNavigate();
+    useEffect(() => {
+        View_Target_Astrologer(id).then(data => {
+            setastrologerList(data.pediatricians[0]);
+        }).catch(err => {
+            console.log(err.error)
+        })
+    }, []);
 
+    useEffect(async () => {
+        console.log(astrologerList);
+        // console.log(astrologerList.name);
+    }, [astrologerList]);
+    const mothersList = [
+        {
+            name: 'Name',
+            topic: astrologerList.name,
+
+        },
+        {
+            name: 'NIC',
+            topic: astrologerList.NIC,
+        },
+        {
+            name: 'Address ',
+            topic: astrologerList.address,
+        },
+        {
+            name: 'Contact No',
+            topic: astrologerList.phone_number,
+        },
+        {
+            name: 'Email',
+            topic: astrologerList.email,
+        },
+
+        {
+            name: 'Years of experience',
+            topic: astrologerList.Years_of_experience
+            ,
+        },
+        {
+            name: 'Service charge',
+            topic: astrologerList.service_charge
+            ,
+        }
+
+
+    ];
+    const handleClose = () => {
+        setOpen(false);
+    };
+    const Accept = () => {
+
+        AcceptAstrologer(id).then(data => {
+            // console.log("data -- "+data);
+            navigate(-1);
+        }).catch(err => {
+            console.log(err.error)
+        })
+    };
+    const handleRejectClick= () => {
+        setOpen(true);
+    };
+    const Reject= () => {
+
+        RejectAstrologer(id).then(data => {
+            // console.log("data -- "+data);
+            navigate(-1);
+        }).catch(err => {
+            console.log(err.error)
+        })
+    };
     return (
-        <Card elevation={3} sx={{ pt: '20px', mb: 3 }}>
-            <CardHeader>
-                <Title> User Details </Title>
-            </CardHeader>
+        <div>
+            <Dialog  open={open} onClose={handleClose}>
+                <DialogTitle>Reject Astrologer</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        Are you sure about this action?
+                    </DialogContentText>
 
-            <Box overflow="auto">
-                <ProductTable>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={Reject}>Yes</Button>
+                    <Button onClick={handleClose}>No</Button>
+                </DialogActions>
+            </Dialog>
 
-
-                    <TableBody>
-                        {mothersList.map((postItem, index) => (
-                            <TableRow key={index} hover>
-                                <TableCell colSpan={3} align="left" sx={{ px: 0, textTransform: 'capitalize' }}>
-                                    <Box display="flex" alignItems="center">
-                                        <Paragraph sx={{ m: 0, ml: 4 }}>{postItem.name}</Paragraph>
-                                    </Box>
-                                </TableCell>
-
-                                <TableCell align="left" colSpan={3} sx={{ px: 0, textTransform: 'capitalize' }}>
-                                    { postItem.topic }
-                                </TableCell>
+            <Card elevation={3} sx={{ pt: '20px', mb: 3,mt:2,ml:2}}>
+                <CardHeader sx={{  ml: 3 }}>
+                    <Title sx={{fontSize: 34}}> User Details </Title>
+                </CardHeader>
 
 
+                <Box overflow="auto">
+                    <ProductTable>
 
-                            </TableRow>
-                        ))}
-                    </TableBody>
-                </ProductTable>
-                <div style={{display:"flex",marginLeft:43,marginBottom:30}} >
 
-                    <div className='m-1'>
-                        <Button variant="contained" color="success">Accept</Button>
+                        <TableBody>
+                            {mothersList.map((postItem, index) => (
+                                <TableRow key={index} hover>
+                                    <TableCell colSpan={3} align="left" sx={{ px: 0, textTransform: 'capitalize' }}>
+                                        <Box display="flex" alignItems="center">
+                                            <Paragraph sx={{ m: 0, ml: 4 }}>{postItem.name}</Paragraph>
+                                        </Box>
+                                    </TableCell>
+
+                                    <TableCell align="left" colSpan={3} sx={{ px: 0, textTransform: 'capitalize' }}>
+                                        { postItem.topic }
+                                    </TableCell>
+
+
+
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </ProductTable>
+                    <div style={{display:"flex",marginLeft:43,marginBottom:30, marginTop:10}} >
+
+                        <div className='m-1'>
+                            <Button variant="contained" color="success" onClick={Accept} >Accept</Button>
+                        </div>
+
+                        <div className='m-1'>
+                            <Button variant="contained" color="error" onClick={handleRejectClick} >Reject</Button>
+                        </div>
+
                     </div>
-
-                    <div className='m-1'>
-                        <Button variant="contained" color="error">Reject</Button>
-                    </div>
-                    <div className='m-1'>
-                        <Button variant="contained" color="error">Contact user</Button>
-                    </div>
-
-                </div>
-            </Box>
-        </Card>
+                </Box>
+            </Card>
+        </div>
 
 
 
     );
 };
 
-const mothersList = [
-    {
-        name: 'Name',
-        topic: "Hansana Ranaweera",
 
-    },
-    {
-        name: 'NIC',
-        topic: "199910923456",
-    },
-    {
-        name: 'Birth Date',
-        topic: "1999/10/23",
-    },
-    {
-        name: 'Address ',
-        topic: "No.53 Akuressa",
-    },
-    {
-        name: 'Contact No',
-        topic: "0713805999",
-    },
-    {
-        name: 'Email',
-        topic: "hansna@gmail.com",
-    },
-    {
-        name: 'Currently working at',
-        topic: "Home",
-    },
-    {
-        name: 'Qualifications',
-        topic: "Empty",
-    },
-
-];
 
 export default AstrologerTable;
 
